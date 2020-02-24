afeddd24fb9587b465d8938c9f608a7e1b16c36e
Raghu Betina <raghu@firstdraft.com>
2020-02-24 16:39:49 +0000

    Generate comments resource with draft:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  # Routes for the Comment resource:
+
+  # CREATE
+  get("/comments/new", { :controller => "comments", :action => "new_form" })
+  post("/create_comment", { :controller => "comments", :action => "create_row" })
+
+  # READ
+  get("/comments", { :controller => "comments", :action => "index" })
+  get("/comments/:id_to_display", { :controller => "comments", :action => "show" })
+
+  # UPDATE
+  get("/comments/:prefill_with_id/edit", { :controller => "comments", :action => "edit_form" })
+  post("/update_comment/:id_to_modify", { :controller => "comments", :action => "update_row" })
+
+  # DELETE
+  get("/delete_comment/:id_to_remove", { :controller => "comments", :action => "destroy_row" })
+
+  #------------------------------
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
